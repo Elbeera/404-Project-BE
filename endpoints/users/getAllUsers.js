f3e241e8f97c117709109bd4764d466ba373e05a
Hothyfa Elbeera <h.s.elbeera@gmail.com>
2021-11-02 20:25:27 +0000

    getAllUsers endpoint complete

---
+++ endpoints/users/getAllUsers.js
@@ -0,0 +1,41 @@
+"use strict";
+const AWS = require("aws-sdk");
+
+AWS.config.update({ region: "eu-west-2" });
+
+exports.handler = async (event, context) => {
+  const dbb = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
+  const documentClient = new AWS.DynamoDB.DocumentClient({
+    region: "eu-west-2",
+  });
+
+  const params = {
+    TableName: "User-Data",
+    Key: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  };
+
+  let users = "";
+  let statusCode = 0;
+
+  try {
+    const data = await documentClient.scan(params).promise();
+    users = data;
+    statusCode = 200;
+    console.log(data);
+  } catch (err) {
+    users = "Unable to get users, please try again";
+    statusCode = 500;
+    console.log(err);
+  }
+
+  const response = {
+    statusCode: statusCode,
+    allUsers: users,
+  };
+
+  return response;
+};
