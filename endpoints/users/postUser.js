21bdbd349b019a3b6a853c6ffee959aad1b3e9ff
johnbearfc <jclleggett@gmail.com>
2021-11-04 10:41:26 +0000

    set up cognito response

--- endpoints/users/postUser.js
+++ endpoints/users/postUser.js
@@ -3,37 +3,47 @@ const AWS = require("aws-sdk");
 
 AWS.config.update({ region: "eu-west-2" });
 
-exports.handler = async (event, context) => {
+exports.handler = async (event, context, callback) => {
   const dbb = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
   const documentClient = new AWS.DynamoDB.DocumentClient({
     region: "eu-west-2",
   });
-  const { username, email, password } = JSON.parse(event.body);
+
+  event.response.autoConfirmUser = true;
+
+  if (event.request.userAttributes.hasOwnProperty("email")) {
+    event.response.autoVerifyEmail = true;
+  }
+
+  callback(null, event);
+
+  const userEvent = event.request;
 
   const params = {
     TableName: "User-Data",
     Item: {
-      email: email,
-      username: username,
-      password: password,
+      // email: userEvent.UserAttributes.email,
+      username: userEvent.Username,
       userPlants: [],
     },
+    ReturnValues: "ALL_NEW",
   };
 
-  let user = "";
+  let newUser = "";
   let statusCode = 0;
 
   try {
     const data = await documentClient.put(params).promise();
-    user = JSON.stringify(data.Item);
+    newUser = data.Item;
     statusCode = 201;
   } catch (err) {
-    user = "Unable to create user, please try again";
+    newUser = "Unable to create user, please try again";
     if (err.statusCode) {
       statusCode = err.statusCode;
     } else {
       statusCode = 500;
     }
+    console.log(err);
   }
 
   const response = {
@@ -41,7 +51,7 @@ exports.handler = async (event, context) => {
     headers: {
       my_header: "my_value",
     },
-    body: user,
+    body: JSON.stringify(newUser),
     isBase64Encoded: false,
   };
 
