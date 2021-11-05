4356164c396c154506e1dc62413d1b2635f54801
Melissa Southern <melissa_southern@hotmail.co.uk>
2021-11-05 12:00:14 +0000

    added GET /plants?category=category test

--- __tests__/endpointTests.js
+++ __tests__/endpointTests.js
@@ -32,3 +32,37 @@ describe("GET /plants", () => {
     });
   });
 });
+
+describe("GET /plants?category=category", () => {
+  test("200: responds with an array of all Flowering House Plants", async () => {
+    const result = await request(app)
+      .get("/plants?category=Flowering House Plants")
+      .expect(200);
+    console.log(result.body);
+    expect(result.body.length).toBe(92);
+    result.body.forEach((plant) => {
+      expect(plant).toMatchObject({
+        commonName: expect.any(String),
+        wiki: expect.any(String),
+        careDetails: {
+          min_soil_moist: expect.any(Number),
+          min_temp: expect.any(Number),
+          max_env_humid: expect.any(Number),
+          min_env_humid: expect.any(Number),
+          min_light_lux: expect.any(Number),
+          min_soil_ec: expect.any(Number),
+          max_light_mmol: expect.any(Number),
+          max_light_lux: expect.any(Number),
+          max_soil_ec: expect.any(Number),
+          max_temp: expect.any(Number),
+          min_light_mmol: expect.any(Number),
+          max_soil_moist: expect.any(Number),
+        },
+        category: expect.any(String),
+        botanicalName: expect.any(String),
+        image_url: expect.any(String),
+        description: expect.any(String),
+      });
+    });
+  });
+});
