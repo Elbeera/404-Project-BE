d6ca33d2c996705fa544b355397b4fdc68f22b73
Melissa Southern <melissa_southern@hotmail.co.uk>
2021-11-05 11:36:45 +0000

    testing files set up, first test added

---
+++ __tests__/endpointTests.js
@@ -0,0 +1,34 @@
+const { expect } = require("@jest/globals");
+const request = require("supertest");
+const app = "https://l81eyc3fja.execute-api.eu-west-2.amazonaws.com/beta";
+
+describe("GET /plants", () => {
+  test("200: responds with an array of all plants", async () => {
+    const result = await request(app).get("/plants").expect(200);
+    expect(result.body.length).toBe(223);
+    result.body.forEach((plant) => {
+      expect(plant).toMatchObject({
+        commonName: expect.any(String),
+        wiki: expect.any(String),
+        careDetails: {
+          min_soil_moist: expect.any(Number),
+          min_temp: expect.any(Number),
+          max_env_humid: expect.any(Number),
+          min_env_humid: expect.any(Number),
+          min_light_lux: expect.any(Number),
+          min_soil_ec: expect.any(Number),
+          max_light_mmol: expect.any(Number),
+          max_light_lux: expect.any(Number),
+          max_soil_ec: expect.any(Number),
+          max_temp: expect.any(Number),
+          min_light_mmol: expect.any(Number),
+          max_soil_moist: expect.any(Number),
+        },
+        category: expect.any(String),
+        botanicalName: expect.any(String),
+        image_url: expect.any(String),
+        description: expect.any(String),
+      });
+    });
+  });
+});
